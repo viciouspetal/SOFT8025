741c213d26b72858b14f61d363b42163c52aabe9
viciouspetal <joanna.wojcik@mycit.ie>
2018-03-12 18:42:29 +0000

    Cart changes (#8)
    * added package json for the whole project for easy setup and startup
    
    * added new product endpoint
    
    * added new product endpoint
    
    * Added currency to cart, product and total. Added element style to cart total
    
    * Fixing param name
    
    * cart changes. Fixing git auto merge reviving old unstyled total
    
    * cart changes. Fixing git auto merge reviving old unstyled total

--- cart/app.js
+++ cart/app.js
@@ -2,7 +2,6 @@ var express = require("express")
     , morgan = require("morgan")
     , path = require("path")
     , bodyParser = require("body-parser")
-
     , app = express();
 
 
@@ -17,11 +16,8 @@ app.post("/add", function (req, res, next) {
     var obj = req.body;
     console.log("add ");
     console.log("Attempting to add to cart: " + JSON.stringify(req.body));
-
-
-    //  var obj = JSON.parse(body);
-
-    //       console.log('addToCart id '+id)
+    // var obj = JSON.parse(body);
+    // console.log('addToCart id '+id)
     var max = 0;
     var ind = 0;
     if (cart["" + obj.custId] === undefined)
@@ -40,10 +36,22 @@ app.post("/add", function (req, res, next) {
         "quantity": obj.quantity
     };
     console.log(JSON.stringify(data));
-    c.push(data);
+
+    // finding the copies of the product in the existing cart object. Then summing up no of occurrences to form quantity var.
+    var found = false;
+
+    c.forEach(function(product) {
+        if(product.productID === obj.productID){
+            found = true;
+            product.quantity = parseInt(product.quantity) + parseInt(obj.quantity);
+        }
+    });
+
+    if(!found){
+        c.push(data);
+    }
 
     res.status(201);
-
     res.send("");
 
 
@@ -55,10 +63,6 @@ app.delete("/cart/:custId/items/:id", function (req, res, next) {
     console.log("Delete item from cart: for custId " + req.url + ' ' +
         req.params.id.toString());
     console.log("delete ");
-
-
-
-
 
     res.send(' ');
 
